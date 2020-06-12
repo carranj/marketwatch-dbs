511d1a5ecea005ebaf65d524458975fbb60c24e4
carranj <carranco.jose.r@gmail.com>
2020-06-12 10:12:48 -0500

    start on bringing in bearer token

--- scripts/config/bearerToken.js
+++ scripts/config/bearerToken.js
@@ -1,30 +1,31 @@
-const { curly } = require('node-libcurl');
-const https = require('https');
+const axios = require('axios');
 const apiConfig = require(".");
 
 const publicKey = apiConfig.TCG_PUBLIC_KEY;
 const privateKey = apiConfig.TCG_PRIVATE_KEY;
-
-const fields = `grant_type=client_credentials&client_id=${publicKey}&client_secret=${privateKey}`;
 const tokenURL = "https://api.tcgplayer.com/token";
-const headers = ["application/x-www-form-urlencoded"];
 
 //Print Error Messages
 function printError(error){
     console.error(error.message);
 }
 
-
-getBearerToken = async function() { 
-    try{
-        const request = await curly.post(tokenURL, {
-            POSTFIELDS: fields,
-            HTTPHEADER: headers
-        });
-        bearerToken = request.data
-        console.log(bearerToken);
-        return bearerToken;
-    } catch (error){
+getBearerToken = function() {
+    axios({
+        "method": "POST",
+        "url": tokenURL,
+        "headers": {
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+        },
+        "data": `grant_type=client_credentials&client_id=${publicKey}&client_secret=${privateKey}`
+    })
+    .then(function (response) {
+        accessToken = response.data.access_token;
+    })
+    .catch(function (error) {
+        // handle error
         printError(error);
-    }
+    })
 }
+
+getBearerToken();
--- scripts/products-by-sets.js
+++ scripts/products-by-sets.js
@@ -2,7 +2,8 @@ const { curly, Curl } = require('node-libcurl');
 const apiConfig = require("./config");
 const bearerConfig = require("./config/bearerToken");
 
-bearerToken = getBearerToken();
+bearerToken =  getBearerToken();
+console.log(bearerToken);
 const publicKey = apiConfig.TCG_PUBLIC_KEY;
 const privateKey = apiConfig.TCG_PRIVATE_KEY;
 
@@ -10,36 +11,24 @@ const fields = `grant_type=client_credentials&client_id=${publicKey}&client_secr
 const tokenURL = "https://api.tcgplayer.com/token";
 let headers = ["application/x-www-form-urlencoded"];
 
-getBearerToken = async function() { 
-  try{
-    const request = await curly.post(tokenURL, {
-        POSTFIELDS: fields,
-        HTTPHEADER: headers
-    });
-  } catch (error){
-    console.log(new Error(error));
-  }
-}
-headers = 
-  [
-    "Accept: application/json",
-  	"Content-Type: application/json",
-  	`Authorization: bearer ${bearerToken}`
-  ];
-console.log(headers);
-//Set Game Name
+
 const categoryId = 27; //ID for DBS
+const groupId = 2590; //Universal Onslaught Test
 const productName = "Dragon Ball Super CCG";
 
 const curl = new Curl();
 
-const requestListAllCategoryRarities = `http://api.tcgplayer.com/v1.32.0/catalog/categories/${categoryId}/printings`; // Request Get
+const requestListAllCategoryRarities = `http://api.tcgplayer.com/v1.32.0/catalog/products?categoryId=${categoryId}&groupId=${groupId}&limit=10000`; // Request Get
+
 curl.setOpt('URL', requestListAllCategoryRarities);
 curl.setOpt('HTTPHEADER', headers);
- 
+    
 curl.on('end', function (statusCode, data, headers) {
+  console.info(statusCode);
+  console.info('---');
+  console.info(data);
+  console.info('---');
   console.info(this.getInfo( 'TOTAL_TIME'));
+
   this.close();
 });
-curl.on('error', curl.close.bind(curl));
-curl.perform();
